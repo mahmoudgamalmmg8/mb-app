221dca57d04c50b18ef6ddae532d1faabeacf015
mahmoudgamalmmg8 <44068022+mahmoudgamalmmg8@users.noreply.github.com>
2022-01-28 19:28:02 +0200

    added tests

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from cgitb import text
 from django.db import models
 
 # Create your models here.
-class post(models.Model):
+class Post(models.Model):
     text = models.TextField()
     def __str__(self):
         return self.text[:50]   
--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,9 @@ from importlib.resources import contents
 from re import template
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import post
+from .models import Post
 # Create your views here.
 class HomePageView(ListView):
-    model = post
+    model = Post
     template_name = 'home.html'
     context_object_name = 'all_posts_lists'
